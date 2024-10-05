386c600f85c04d8dbaebf957bd19224b81b32ec6
tyang816 <ty_ang@163.com>
2024-10-05 22:24:19 +0800

    Add structure align support

--- compute_fitness.py
+++ compute_fitness.py
@@ -1,6 +1,7 @@
 
 import torch
 import os
+import json
 import pandas as pd
 from tqdm import tqdm
 from pathlib import Path
@@ -9,6 +10,28 @@ from scipy.stats import spearmanr
 from transformers import AutoTokenizer, AutoModelForMaskedLM
 from argparse import ArgumentParser
 
+amino_acid_properties = {
+    'A': {'hydrophobicity': 1.8,  'charge':  0, 'polarity':  0, 'molecular_weight':  89.09, 'volume':  88.6},
+    'R': {'hydrophobicity': -4.5, 'charge': +1, 'polarity':  1, 'molecular_weight': 174.20, 'volume': 173.4},
+    'N': {'hydrophobicity': -3.5, 'charge':  0, 'polarity':  1, 'molecular_weight': 132.12, 'volume': 114.1},
+    'D': {'hydrophobicity': -3.5, 'charge': -1, 'polarity':  1, 'molecular_weight': 133.10, 'volume': 111.1},
+    'C': {'hydrophobicity': 2.5,  'charge':  0, 'polarity':  0, 'molecular_weight': 121.15, 'volume': 108.5},
+    'Q': {'hydrophobicity': -3.5, 'charge':  0, 'polarity':  1, 'molecular_weight': 146.15, 'volume': 143.8},
+    'E': {'hydrophobicity': -3.5, 'charge': -1, 'polarity':  1, 'molecular_weight': 147.13, 'volume': 138.4},
+    'G': {'hydrophobicity': -0.4, 'charge':  0, 'polarity':  0, 'molecular_weight':  75.07, 'volume':  60.1},
+    'H': {'hydrophobicity': -3.2, 'charge':  0, 'polarity':  1, 'molecular_weight': 155.16, 'volume': 153.2},
+    'I': {'hydrophobicity': 4.5,  'charge':  0, 'polarity':  0, 'molecular_weight': 131.17, 'volume': 166.7},
+    'L': {'hydrophobicity': 3.8,  'charge':  0, 'polarity':  0, 'molecular_weight': 131.17, 'volume': 166.7},
+    'K': {'hydrophobicity': -3.9, 'charge': +1, 'polarity':  1, 'molecular_weight': 146.19, 'volume': 168.6},
+    'M': {'hydrophobicity': 1.9,  'charge':  0, 'polarity':  0, 'molecular_weight': 149.21, 'volume': 162.9},
+    'F': {'hydrophobicity': 2.8,  'charge':  0, 'polarity':  0, 'molecular_weight': 165.19, 'volume': 189.9},
+    'P': {'hydrophobicity': -1.6, 'charge':  0, 'polarity':  0, 'molecular_weight': 115.13, 'volume': 112.7},
+    'S': {'hydrophobicity': -0.8, 'charge':  0, 'polarity':  1, 'molecular_weight': 105.09, 'volume':  89.0},
+    'T': {'hydrophobicity': -0.7, 'charge':  0, 'polarity':  1, 'molecular_weight': 119.12, 'volume': 116.1},
+    'W': {'hydrophobicity': -0.9, 'charge':  0, 'polarity':  0, 'molecular_weight': 204.23, 'volume': 227.8},
+    'Y': {'hydrophobicity': -1.3, 'charge':  0, 'polarity':  1, 'molecular_weight': 181.19, 'volume': 193.6},
+    'V': {'hydrophobicity': 4.2,  'charge':  0, 'polarity':  0, 'molecular_weight': 117.15, 'volume': 140.0},
+}
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
 
@@ -26,7 +49,7 @@ def read_multi_fasta(file_path):
             line = line.strip()
             if line.startswith('>'):
                 if current_sequence:
-                    sequences[header] = current_sequence.upper()
+                    sequences[header] = current_sequence.upper().replace('-', '<pad>')
                     current_sequence = ''
                 header = line
             else:
@@ -40,7 +63,7 @@ def read_seq(fasta):
     for record in SeqIO.parse(fasta, "fasta"):
         return str(record.seq)
 
-def count_matrix_from_alignment(tokenizer, alignment_file):
+def count_matrix_from_residue_alignment(tokenizer, alignment_file):
     alignment_dict = read_multi_fasta(alignment_file)
     aln_start, aln_end = list(alignment_dict.keys())[0].split('/')[-1].split('-')
     alignment_seqs = list(alignment_dict.values())
@@ -53,6 +76,35 @@ def count_matrix_from_alignment(tokenizer, alignment_file):
         count_matrix[i] = torch.bincount(alignment_ids[:,i], minlength=tokenizer.vocab_size)
     count_matrix = (count_matrix / count_matrix.sum(dim=1, keepdim=True)).to(device)
     return count_matrix, int(aln_start)-1, int(aln_end)
+
+
+def count_matrix_from_structure_alignment(tokenizer, alignment_file):
+    alignment_dict = read_multi_fasta(alignment_file)
+    alignment_seqs = list(alignment_dict.values())
+    print(">>> Start tokenizer")
+    tokenized_results = tokenizer([alignment_seqs[0]], return_tensors="pt", padding=True)
+    alignment_ids = tokenized_results["input_ids"][:,1:-1]
+    # count distribution of each column, [seq_len, vocab_size]
+    count_matrix = torch.zeros(alignment_ids.size(1), tokenizer.vocab_size)
+    for i in tqdm(range(alignment_ids.size(1))):
+        count_matrix[i] = torch.bincount(alignment_ids[:,i], minlength=tokenizer.vocab_size)
+    count_matrix = (count_matrix / count_matrix.sum(dim=1, keepdim=True)).to(device)
+    return count_matrix
+    
+
+
+def calculate_property_difference(wild_aa, mutant_aa, weights=None):
+    properties = amino_acid_properties[wild_aa].keys()
+    if weights is None:
+        weights = {prop: 1 for prop in properties}
+    differences = []
+    for prop in properties:
+        wild_value = amino_acid_properties[wild_aa][prop]
+        mutant_value = amino_acid_properties[mutant_aa][prop]
+        difference = abs(mutant_value - wild_value)
+        weighted_diff = weights.get(prop, 1) * difference
+        differences.append(weighted_diff)
+    return differences
 
 def tokenize_structure_sequence(structure_sequence):
     shift_structure_sequence = [i + 3 for i in structure_sequence]
@@ -67,7 +119,7 @@ def tokenize_structure_sequence(structure_sequence):
 
 @torch.no_grad()
 def score_protein(model, tokenizer, residue_fasta, structure_fasta, mutant_df, alpha=0.7,
-                  residu_alignment_file=None):
+                  residu_alignment_file=None, structure_alignment_file=None):
     sequence = read_seq(residue_fasta)
     structure_sequence = read_seq(structure_fasta)
 
@@ -89,9 +141,14 @@ def score_protein(model, tokenizer, residue_fasta, structure_fasta, mutant_df, a
     
     if residu_alignment_file is not None:
         print(">>> Using residue sequence alignment matrix...")
-        alignment_matrix, aln_start, aln_end = count_matrix_from_alignment(tokenizer, residu_alignment_file)
+        alignment_matrix, aln_start, aln_end = count_matrix_from_residue_alignment(tokenizer, residu_alignment_file)
         aln_modify_logits = (1-alpha) * logits[aln_start: aln_end, :] + alpha * torch.log_softmax(alignment_matrix, dim=-1)
         logits = torch.cat([logits[:aln_start], aln_modify_logits, logits[aln_end:]], dim=0)
+
+    if structure_alignment_file is not None:
+        print(">>> Using structure sequence alignment matrix...")
+        alignment_matrix = count_matrix_from_structure_alignment(tokenizer, structure_alignment_file)
+        logits = (1-alpha) * logits + alpha * torch.log_softmax(alignment_matrix, dim=-1)
 
     mutants = mutant_df["mutant"].tolist()
     scores = []
@@ -113,9 +170,9 @@ def read_names(fasta_dir):
     files = Path(fasta_dir).glob("*.fasta")
     names = [file.stem for file in files]
     return names
+    
 
-
-def main():
+if __name__ == "__main__":
     parser = ArgumentParser()
     parser.add_argument("--model_name", type=str, default="AI4Protein/ProSST-2048", nargs="+", required=True)
     parser.add_argument("--model_out_name", type=str, default="ProSST-2048", nargs="+", help="Output model name",)
@@ -127,9 +184,10 @@ def main():
     parser.add_argument("--mutant_dir", type=str, default=None, help="Directory containing CSV files with mutants",)
     
     # retrieval and logits mode
-    parser.add_argument("--retrieval_mode", type=str, default=None, choices=["seq", "struc", "seq+struc"], help="Mode to retrieve data",)
+    parser.add_argument("--logit_mode", type=str, default=None, choices=["aa_seq_aln", "struc_seq_aln", "aa_seq_aln+struc_seq_aln", "aa_seq_prop"], help="Mode to retrieve data",)
     parser.add_argument("--alpha", type=float, default=0.5, help="Alpha value for combining logits",)
     parser.add_argument("--residue_alignment_dir", type=str, default=None, help="Directory containing a2m files of residue alignments",)
+    parser.add_argument("--structure_alignment_dir", type=str, default=None, help="Directory containing fasta files of foldseek structure alignments",)
     
     # output directory
     parser.add_argument("--out_scores_dir", default=None, help="Directory to save scores")
@@ -162,10 +220,16 @@ def main():
                 residue_fasta = f"{args.base_dir}/residue_sequence/{protein_name}.fasta"
                 structure_fasta = f"{args.base_dir}/structure_sequence/{model_name.split('-')[-1]}/{protein_name}.fasta"
                 mutant_file = f"{args.base_dir}/substitutions/{protein_name}.csv"
-                if args.retrieval_mode == "seq":
+                if "aa_seq_aln" in args.logit_mode:
                     residu_alignment_file = f"{args.base_dir}/residue_alignment/{protein_name}.a2m"
                 else:
                     residu_alignment_file = None
+                
+                if "struc_seq_aln" in args.logit_mode:
+                    structure_alignment_file = f"{args.base_dir}/structure_alignment/{protein_name}.fasta"
+                else:
+                    structure_alignment_file = None
+                    
             if args.residue_dir:
                 residue_fasta = f"{args.residue_dir}/{protein_name}.fasta"
             if args.structure_dir:
@@ -181,26 +245,31 @@ def main():
             else:
                 model_out_name = model_name.split("/")[-1]
                 
-            if model_out_name not in mutant_df.columns:
-                scores = score_protein(
-                        model=model,
-                        tokenizer=tokenizer,
-                        residue_fasta=residue_fasta,
-                        structure_fasta=structure_fasta,
-                        mutant_df=mutant_df,
-                        alpha=args.alpha,
-                        residu_alignment_file=residu_alignment_file,
-                    )
-                
-                mutant_df[model_out_name] = scores
+            # if model_out_name not in mutant_df.columns:
+            scores = score_protein(
+                    model=model,
+                    tokenizer=tokenizer,
+                    residue_fasta=residue_fasta,
+                    structure_fasta=structure_fasta,
+                    mutant_df=mutant_df,
+                    alpha=args.alpha,
+                    residu_alignment_file=residu_alignment_file,
+                    structure_alignment_file=structure_alignment_file,
+                )
+            
+            mutant_df[model_out_name] = scores
             
             corr = spearmanr(mutant_df["DMS_score"], mutant_df[model_out_name]).correlation
             corrs.append(corr)
             print(f">>> {model_name} on {protein_name}: {corr}")
             print("=====================================")
             mutant_df.to_csv(f"{args.out_scores_dir}/scores/{protein_name}.csv", index=False)
+        
         print(f"====== {model_name} average correlation: {sum(corrs)/len(corrs)} ======")
-        pd.DataFrame({'protein': protein_names, model_out_name: corrs}).to_csv(f"{args.out_scores_dir}/correlation.csv", index=False)
-
-if __name__ == "__main__":
-    main()
+        summary_df_path = f"{args.out_scores_dir}/correlation.csv"
+        if os.path.exists(summary_df_path):
+            summary_df = pd.read_csv(summary_df_path)
+            summary_df[model_out_name] = corrs
+        else:
+            summary_df = pd.DataFrame({'protein': protein_names, model_out_name: corrs})
+        summary_df.to_csv(f"{args.out_scores_dir}/correlation.csv", index=False)
--- script/compute_fitness.sh
+++ script/compute_fitness.sh
@@ -5,12 +5,20 @@ CUDA_VISIBLE_DEVICES=0 python compute_fitness.py \
     --base_dir data/proteingym_v1 \
     --out_scores_dir result/proteingym_v1
 
+export HF_ENDPOINT=https://hf-mirror.com
+CUDA_VISIBLE_DEVICES=0 python compute_fitness.py \
+    --model_name AI4Protein/ProSST-2048 \
+    --logit_mode aa_seq_aln \
+    --alpha 0.5 \
+    --model_out_name ProSST-2048-seq_aln \
+    --base_dir data/proteingym_v1 \
+    --out_scores_dir result/proteingym_v1
 
 export HF_ENDPOINT=https://hf-mirror.com
 CUDA_VISIBLE_DEVICES=3 python compute_fitness.py \
     --model_name AI4Protein/ProSST-2048 \
-    --retrieval_mode seq \
-    --alpha 0.3 \
-    --model_out_name ProSST-2048-seq_aln0.3 \
+    --logit_mode struc_seq_aln \
+    --alpha 0.7 \
+    --model_out_name ProSST-2048-struc_aln0.7 \
     --base_dir data/proteingym_v1 \
     --out_scores_dir result/proteingym_v1
