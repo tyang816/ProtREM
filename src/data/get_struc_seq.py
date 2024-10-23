e2250cde1e51240c0e921095a549d20057cee9f2
tyang816 <ty_ang@163.com>
2024-10-23 09:56:15 +0800

    Refactor get_struc_seq.py to support variable vocabulary size

--- src/data/get_struc_seq.py
+++ src/data/get_struc_seq.py
@@ -10,19 +10,19 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("--pdb_dir", type=str, default=None, help="Directory containing PDB files",)
     parser.add_argument("--pdb_file", type=str, default=None, help="PDB file",)
+    parser.add_argument("--vocab_size", type=int, default=[2048], nargs='+', help="Vocabulary size",)
     parser.add_argument("--out_dir", type=str, default=None, help="Output directory",)
     args = parser.parse_args()
     
-    vocab_size = [20, 128, 512, 1024, 2048, 4096]
     os.makedirs(args.out_dir, exist_ok=True)
-    for v in vocab_size:
+    for v in args.vocab_size:
         os.makedirs(os.path.join(args.out_dir, str(v)), exist_ok=True)
     
     if args.pdb_dir is not None:
         pdb_files = os.listdir(args.pdb_dir)
         for pdb_file in tqdm(pdb_files):
             pdb_name = pdb_file.split(".")[0]
-            for v in vocab_size:
+            for v in args.vocab_size:
                 processor = PdbQuantizer(structure_vocab_size=v)
                 result = processor(os.path.join(args.pdb_dir, pdb_file), return_residue_seq=False)
                 result = [str(i) for i in result]
